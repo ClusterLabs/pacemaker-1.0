6fe786e7f1d30b019bb4220d8c65de69a72db7f5
yixiong <devnull@localhost>
2004-10-27 09:27:13 +0000

    This fixes the bug that deals with the situation where the master SNMP agent is not available.  In this case, we return error code 100 so heartbeat will not respawn us.
    CVS patchset: 4214
    CVS date: 2004/10/27 09:27:13
    
    
    Mercurial revision: 5fd70ac48fdbda5dffbc2e5a5911848e97bc490f

--- snmp_subagent/hbagent.c
+++ snmp_subagent/hbagent.c
@@ -1147,7 +1147,11 @@ main(int argc, char ** argv)
 		exit(1);
 
 	/* initialize the agent library */
-	init_agent("LHA-agent");
+	if (!init_agent("LHA-agent")) {
+		cl_log(LOG_ERR, "AgentX initialization failure.  This is unrecoverable.  Make sure that the master snmpd is started and is accepting AgentX connetions.  The subagent will not be respawned.");
+
+		return 100;
+	}
 
 	/* initialize mib code here */
 
