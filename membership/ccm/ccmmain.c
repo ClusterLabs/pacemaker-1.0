ccd1ef1fc41b11a84397522d011129ebd890d9d8
gshi <devnull@localhost>
2005-04-05 19:51:53 +0000

    enable using logging daemon according to heartbeat's config
    CVS patchset: 5612
    CVS date: 2005/04/05 19:51:53
    
    
    Mercurial revision: 50a77007c5088fab2d32c32d29ec676ab18e90fb

--- membership/ccm/ccmmain.c
+++ membership/ccm/ccmmain.c
@@ -1,4 +1,4 @@
-/* $Id: ccmmain.c,v 1.24 2005/03/22 00:13:22 gshi Exp $ */
+/* $Id: ccmmain.c,v 1.25 2005/04/05 19:51:53 gshi Exp $ */
 /* 
  * ccm.c: Consensus Cluster Service Program 
  *
@@ -206,6 +206,55 @@ ccm_shutdone(int sig, gpointer userdata)
 	
 	return TRUE;
 }
+
+#define ENV_PREFIX "HA_"
+
+static int
+ccm_str_to_boolean(const char * s, int * ret)
+{
+	if(s == NULL) {
+		return -1;
+
+	} else if (strcasecmp(s, "true") == 0
+	||	strcasecmp(s, "on") == 0
+	||	strcasecmp(s, "yes") == 0
+	||	strcasecmp(s, "y") == 0
+	||	strcasecmp(s, "1") == 0){
+		*ret = TRUE;
+		return 1;
+
+	} else if (strcasecmp(s, "false") == 0
+	||	strcasecmp(s, "off") == 0
+	||	strcasecmp(s, "no") == 0
+	||	strcasecmp(s, "n") == 0
+	||	strcasecmp(s, "0") == 0){
+		*ret = FALSE;
+		return 1;
+	}
+	return -1;
+}
+
+static void 
+enable_logd_if_needed(void)
+{
+	const char*	param_name = ENV_PREFIX "" KEY_LOGDAEMON;
+	char*		param_val;
+	gboolean	truefalse = FALSE;
+
+	param_val = getenv(param_name);
+	
+	if(param_val != NULL) {
+		ccm_str_to_boolean(param_val, &truefalse);
+		cl_log_set_uselogd(truefalse) ;
+	}
+	
+	if (truefalse){
+		cl_log(LOG_INFO, "Enable using logging daemon");
+	}else {
+		cl_log(LOG_INFO, "Disable using logging daemon");
+	}
+	
+}
 /* */
 /* The main function! */
 /* */
@@ -251,6 +300,9 @@ main(int argc, char **argv)
 	CL_SIGNAL(SIGUSR2, ccm_debug);
 	CL_IGNORE_SIG(SIGPIPE);
 	
+
+	enable_logd_if_needed();
+
 	/* initialize the client tracking system */
 	client_init();
 
