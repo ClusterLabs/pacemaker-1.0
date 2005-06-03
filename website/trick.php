a6353a0cce4b7d6c576d898d9e31802e2d7ee584
alan <devnull@localhost>
2005-06-03 16:20:20 +0000

    Put in a file I'd forgotten for supporting the web site.
    CVS patchset: 6561
    CVS date: 2005/06/03 16:20:20
    
    
    Mercurial revision: 4e391a4996f3a39c815dd176a1264942af2143fe

---
+++ website/trick.php
@@ -0,0 +1,68 @@
+<?php
+# WikiWare - Standard Semantic Content Switcher
+# Dmytri Kleiner -- dmytrik@trick.ca
+# Lars Ellenberg -- l.g.e@web.de
+#   some adjustments in 2005 for the moved linux ha wiki
+# Copyleft 2003-2004 Idiosyntactix. All rights detourned.	
+
+# Usage Examples:
+#   include "trick.php";
+#   print $content; 
+
+# General Configuration
+$sitename = "Linux HA";
+
+# for my local test wiki
+# $MOINMOINalias = "ha"; # do NOT include leading or trailing slash
+# $MOINMOINserver = "http://localhost";
+
+# for this one:
+$MOINMOINserver = "http://wiki.linux-ha.org";
+$MOINMOINalias = ""; # do NOT include leading or trailing slash
+$MOINMOINurl = "$MOINMOINserver/$MOINMOINalias";
+$MOINMOINcachedir = "_cache";
+$MOINMOINfilemod = 0664;
+#
+#	Images to delete when we see them...
+#
+$MOINMOINExtraneousImages = array(
+	'/wiki/classic/img/moin-www.png',
+	'/wiki/classic/img/moin-ftp.png'
+);
+
+# for http://some.server/WikiPageName
+$local_cache_url_prefix = "/";
+# for http://some.vhost.dedicated.to.transclusion/WikiPageName
+# $local_cache_url_prefix = "/"
+
+#############################################################
+#############################################################
+#############################################################
+
+include "moinmoin.php";
+
+$path_info = $HTTP_SERVER_VARS["PATH_INFO"];
+
+if (strlen($path_info) < 3) { 
+	
+	# Default Home Page
+ $real_path_info = $path_info;	
+	$path_info = "/HomePage";
+}
+
+
+# Wiki Page
+$pagename = substr($path_info,1);
+
+# in case there is some wiki style escaped special char in the pagename derived from the url
+# $pagetitle is only used in the "title" tag, and should be readable
+# (typically displayed as window/tab title by your browser)
+$pagetitle = str_replace(array("_20","_21","_2c", "_2d","_2e","_2f","_27", "_28", "_29", "_3a"), array(" ","!",",","-",".","/","'","(",")",":"), $pagename);
+
+# in case there are some special chars in the page name, that are not yet escaped ...
+# $pagename is used to request that page from the wiki, or to read in the cache file
+$pagename = str_replace(array(" ","!",",","-",".","/","'","(",")",":"), array("_20","_21","_2c", "_2d","_2e","_2f","_27", "_28", "_29", "_3a"), $pagename);
+	
+$content = MoinMoin($pagename);
+
+?>
