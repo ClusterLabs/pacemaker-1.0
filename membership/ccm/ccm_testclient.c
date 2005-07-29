3dc1ace51a05424b8a695a805e27d0f3e026196d
panjiam <devnull@localhost>
2005-07-29 07:27:37 +0000

    updated license statements
    CVS patchset: 7146
    CVS date: 2005/07/29 07:27:37
    
    
    Mercurial revision: ac03d6fc09724b40b2f928419905229f8d90a232

--- membership/ccm/ccm_testclient.c
+++ membership/ccm/ccm_testclient.c
@@ -1,26 +1,25 @@
-/* $Id: ccm_testclient.c,v 1.17 2005/03/16 17:11:15 lars Exp $ */
+/* $Id: ccm_testclient.c,v 1.18 2005/07/29 07:27:37 panjiam Exp $ */
 /* 
  * ccm.c: A consensus cluster membership sample client
  *
  * Copyright (c) International Business Machines  Corp., 2000
  * Author: Ram Pai (linuxram@us.ibm.com)
  * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
 #include <portability.h>
 #include <ocf/oc_event.h>
 #include <stdio.h>
