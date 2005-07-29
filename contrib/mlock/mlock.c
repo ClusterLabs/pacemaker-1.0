d2ef046b2d95bebe269d3bb1499185d41ab00e09
sunjd <devnull@localhost>
2005-07-29 03:15:42 +0000

    bug668: license update
    CVS patchset: 7133
    CVS date: 2005/07/29 03:15:42
    
    
    Mercurial revision: 2bb1e24a2a1ecea84ee049c32adf648202f359df

--- contrib/mlock/mlock.c
+++ contrib/mlock/mlock.c
@@ -1,24 +1,23 @@
-/* $Id: mlock.c,v 1.12 2005/07/13 14:55:41 lars Exp $ */
+/* $Id: mlock.c,v 1.13 2005/07/29 03:15:42 sunjd Exp $ */
 /*
  *
  * multi-clients NFS lock test code
  *
  * Copyright (C) 2004 Guochun Shi<gshi@ncsa.uiuc.edu>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 
