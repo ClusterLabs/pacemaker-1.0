4e67dc95f411b0aed8720caeb79d76bb6fe31901
sunjd <devnull@localhost>
2005-07-29 06:17:44 +0000

    bug668: license update
    CVS patchset: 7140
    CVS date: 2005/07/29 06:17:44
    
    
    Mercurial revision: e362e139085ef1f12f1fc7c80c557a4dd64ce7be

--- telecom/apphbd/apphbtest.c
+++ telecom/apphbd/apphbtest.c
@@ -1,4 +1,4 @@
-/* $Id: apphbtest.c,v 1.24 2005/02/17 09:46:24 sunjd Exp $ */
+/* $Id: apphbtest.c,v 1.25 2005/07/29 06:21:10 sunjd Exp $ */
 /*
  * apphbtest:	application heartbeat test program
  *
@@ -9,19 +9,19 @@
  *
  *********************************************************************
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #include <portability.h>
 #include <stdlib.h>
