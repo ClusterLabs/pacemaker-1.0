40d216db99e0a49145559c12e2b9c7a96c45ac12
panjiam <devnull@localhost>
2005-10-26 13:45:08 +0000

    added pegasus support
    CVS patchset: 7951
    CVS date: 2005/10/26 13:45:08
    
    
    Mercurial revision: 17c3704ee229d382c61f5fb65fff72164dbaf314

--- cim/mof/register.sh
+++ cim/mof/register.sh
@@ -2,38 +2,193 @@
 
 TMP=`mktemp`
 
-sfcb_register()
+#---------- openwbem functions ---------------------------------------
+openwbem_register ()
+{
+        mof_file=$1
+        reg_file=$2
+
+        ps -C owcimomd > /dev/null 2>&1
+        if test $? != 0; then
+                echo "owcimomd not running, start it first"
+                exit
+        fi
+
+        OWMOFC=`which owmofc` 
+        if test $? != 0; then
+                echo "owmofc not found, OpenWbem not installed?"
+        fi
+
+        $OWMOFC $mof_file
+}
+
+openwbem_unregister ()
+{
+        echo "FixME"
+        exit
+}
+
+#---------- pegasus functions ----------------------------------------
+
+pegasus_add_module ()
 {
         mof_file=$1
         reg_file=$2
         out_file=$TMP
 
-        cat $reg_file | grep -v '^[[:space:]]*#.*' | while read classname namespace providername providermodule type
-                do
-                        echo    "############################
-                                [$classname]
-                                provider: $providername
-                                location: $providermodule
-                                type: $type
-                                namespace: $namespace
-                                " >> $out_file
+        modules=`cat $reg_file 2> /dev/null | grep -v '^[[:space:]]*#.*' | cut -d ' ' -f 4 | sort | uniq`
+        for module in $modules; do
+cat >> $out_file <<EOF
+instance of PG_ProviderModule
+{
+   Name = "$module";
+   Location = "$module";
+   Vendor = "LinuxHA";
+   Version = "2.0.0";
+   InterfaceType = "CMPI";
+   InterfaceVersion = "2.0.0";
+};
+EOF
+        done
+
+}
+
+pegasus_add_provider ()
+{
+        mof_file=$1
+        reg_file=$2
+        out_file=$TMP
+
+        providers=`cat $reg_file 2> /dev/null | grep -v '^[[:space:]]*#.*' | cut -d ' ' -f 3-4 | sort | uniq`
+        set -- $providers
+        while test x$1 != x
+        do
+cat >> $out_file <<EOF
+instance of PG_Provider
+{
+   Name = "$1";
+   ProviderModuleName = "$2";
+};
+
+EOF
+                shift 2
+        done
+
+}
+
+
+pegasus_add_capability ()
+{
+        mof_file=$1
+        reg_file=$2
+        out_file=$TMP
+
+        capid=0
+        cat $reg_file | grep -v '^[[:space:]]*#.*' | \
+        while read classname namespace providername providermodule types
+        do
+                capid=`expr $capid + 1`
+                provider_types=""
+                type_no=0
+                for type in $types; do
+                        case $type in
+                                instance)    type_no=2;;
+                                association) type_no=3;;
+                                indication)  type_no=4;;
+                                method)      type_no=5;;
+                                *)           echo "Unknown provider type: $type"
+                                             exit;;
+                        esac
+                        if test "x$provider_types" = "x"; then
+                                provider_types="$type_no"
+                        else
+                                provider_types="$provider_types, $type_no" 
+                        fi
+                done  # for
+
+cat >> $out_file << EOF
+instance of PG_ProviderCapabilities
+{
+   ProviderModuleName = "$providermodule";
+   ProviderName = "$providername";
+   ClassName = "$classname";
+   ProviderType = { $provider_types };
+   Namespaces = {"$namespace"};
+   SupportedProperties = NULL;
+   SupportedMethods = NULL;
+   CapabilityID = "$capid";
+};
+
+EOF
+        done  # while
+
+}
+
+pegasus_register ()
+{
+        mof_file=$1
+        reg_file=$2
+
+        if ps -C cimserver > /dev/null 2>&1
+        then
+                CIMMOF=cimmof
+                state=active
+        else
+                CIMMOF=cimmofl
+                PEGASUSREPOSITORY="/opt/pegasus/repository"
+                CIMMOF="$CIMMOF -R $PEGASUSREPOSITORY"
+                state=inactive
+        fi
+
+        pegasus_add_module $mof_file $reg_file
+        pegasus_add_provider $mof_file $reg_file
+        pegasus_add_capability $mof_file $reg_file
+
+        $CIMMOF -n root/cimv2 $mof_file
+        $CIMMOF -n root/PG_Interop $TMP
+}
+
+pegasus_unregister ()
+{
+        echo "FixME"
+        exit
+}
+
+
+#---------- sfcb functions -------------------------------------------
+sfcb_do_register()
+{
+        mof_file=$1
+        reg_file=$2
+        out_file=$TMP
+
+        cat $reg_file | grep -v '^[[:space:]]*#.*' | \
+        while read classname namespace providername providermodule types
+        do
+cat >> $out_file <<EOF
+#############################
+[$classname]
+provider: $providername
+location: $providermodule
+type: $types
+namespace: $namespace
+#############################
+EOF
         done
 
 
         ## register
-        echo register ...
+        echo register to sfcb ...
         sfcbstage -r $out_file $mof_file
         
-        if [ $? -eq 1 ];
-        then
+        if [ $? -eq 1 ]; then
                 echo failed to register, exit
                 exit
         fi
 
-        echo rebuild ...
+        echo rebuild sfcb repository ...
         sfcbrepos -f
-        if [ $? -eq 1 ];
-        then
+        if [ $? -eq 1 ]; then
                 echo failed to rebuild sfcb, exit
                 exit
         fi
@@ -41,7 +196,7 @@ sfcb_register()
 }
 
 
-sfcb_install () {
+sfcb_register () {
         ps -C sfcbd > /dev/null 2>&1
         if [ $? -eq 0 ];
         then
@@ -49,21 +204,92 @@ sfcb_install () {
                 exit
         fi
 
-        sfcb_register $1 $2
+        sfcb_do_register $1 $2
 
 } 
 
-############ MAIN ###############
-
-if [ ! $# -eq 2 ];
-then 
-        echo "usage:  register.sh MOF REG"
+sfcb_unregister () 
+{
+        echo "FixME"
         exit
+}
+
+#========== main ============================================
+
+mof_file=""
+reg_file=""
+unregister="no"
+cimom=""
+
+usage () {
+        echo "usage: $0 [-t cimserver] [-u] -r regfile -m mof"
+        echo $'\t'-t cimserver: specify cimserver "[pegasus|openwbem|sfcb]".
+        echo $'\t'-u: Unregister 
+        echo $'\t'-r regfile: specify reg file.
+        echo $'\t'-m moffile: specify mof file. 
+ 
+        exit 0
+}
+
+args=`getopt hut:r:m: $*`
+
+
+if test $? != 0; then
+        usage
 fi
 
-mof_file=$1
-reg_file=$2
+while [ -n "$1" ]; do
+        case $1 in
+                -h) usage;;
+                -u) unregister="yes"
+                    shift;;
+                -t) shift
+                    if test "x$cimom" = "x"; then
+                        cimom=$1
+                    else
+                        echo CIM server already set, ignore [$1]
+                    fi
+                    shift;;
+                -m) shift
+                    if test "x$mof_file" = "x"; then
+                        mof_file=$1
+                    else
+                        echo MOF file already set, ignore [$1]
+                    fi
+                    shift;;
+                -r) shift
+                    if test "x$reg_file" = "x"; then
+                        reg_file=$1
+                    else
+                        echo Reg file already set, ignore [$1]
+                    fi
+                    shift;;
+                --) break;;
+        esac
+done
 
 
-sfcb_install $mof_file $reg_file
+echo CIMOM: $cimom
+echo MOF file:  $mof_file
+echo Reg file:  $reg_file
+echo Unregister? $unregister
+
+case $cimom in
+        pegasus) ;;
+        sfcb)    ;;
+        openwbem);;
+        *)  usage;;
+esac
+
+REG_FUNC=${cimom}_register
+UNREG_FUNC=${cimom}_unregister
+
+if test "x$unregister" != "xyes"; then
+        $REG_FUNC $mof_file $reg_file
+else
+        $REG_UNFUNC $mof_file $reg_file
+fi
+ 
 rm -rf $TMP
+
+#============== end of file =======================================
