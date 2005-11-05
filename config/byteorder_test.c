4b17854a9ae61549840bb32fddba6844073f919c
panjiam <devnull@localhost>
2005-11-05 02:39:22 +0000

    made the test simple
    CVS patchset: 8081
    CVS date: 2005/11/05 02:39:22
    
    
    Mercurial revision: 26af8be4a7cbacaad06d78afc51c93a5843667c4

--- config/byteorder_test.c
+++ config/byteorder_test.c
@@ -1,29 +1,11 @@
-#include <stdint.h> 
 #include <stdio.h>
-
-static inline void
-byte_swap(uint32_t * buf, uint32_t len)
-{
-        int i;
-        for (i = 0; i < len; i ++) {
-                uint32_t tmp = buf[i];
-                buf[i] = ( (uint32_t) ((unsigned char *) &tmp)[0] ) |
-                         (((uint32_t) ((unsigned char *) &tmp)[1]) << 8) |
-                         (((uint32_t) ((unsigned char *) &tmp)[2]) << 16) |
-                         (((uint32_t) ((unsigned char *) &tmp)[3]) << 24);
-        }
-}
-
 
 int
 main ()
 {
-	uint32_t a = 10;
-	uint32_t b = a;
+	unsigned int a = 0x1234;
 
-	byte_swap(&a, 1);
-
-	if ( a == b ) {
+	if ( (unsigned int) ( ((unsigned char *)&a)[0]) == 0x34 ) {
 		printf("little-endian\n");
 		return 0;
 	} else {
