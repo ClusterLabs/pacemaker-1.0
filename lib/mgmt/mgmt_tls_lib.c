9827d71fa79ef921c12d596455afb5e935dc36b7
alan <devnull@localhost>
2005-12-21 08:18:01 +0000

    Thought of a better way to do the int->pointer casts I needed to do.
    CVS patchset: 8320
    CVS date: 2005/12/21 08:18:01
    
    
    Mercurial revision: 4fbbb60eef2fce13a8d57efa8045c6c600194c16

--- lib/mgmt/mgmt_tls_lib.c
+++ lib/mgmt/mgmt_tls_lib.c
@@ -30,6 +30,7 @@
 #include <unistd.h>
 #include <gnutls/gnutls.h>
 #include <mgmt/mgmt_tls.h>
+#include <glib.h>
 
 #define DH_BITS 1024
 
@@ -60,7 +61,7 @@ tls_attach_client(int sock)
 	gnutls_set_default_priority(*session);
 	gnutls_kx_set_priority (*session, kx_prio);
 	gnutls_credentials_set(*session, GNUTLS_CRD_ANON, anoncred_client);
-	gnutls_transport_set_ptr(*session, (gnutls_transport_ptr_t) (long)sock);
+	gnutls_transport_set_ptr(*session, (gnutls_transport_ptr_t) GINT_TO_POINTER(sock));
 	ret = gnutls_handshake(*session);
 	if (ret < 0) {
 		fprintf(stderr, "*** Handshake failed\n");
@@ -134,7 +135,7 @@ tls_attach_server(int sock)
 	gnutls_kx_set_priority (*session, kx_prio);
 	gnutls_credentials_set(*session, GNUTLS_CRD_ANON, anoncred_server);
 	gnutls_dh_set_prime_bits(*session, DH_BITS);
-	gnutls_transport_set_ptr(*session, (gnutls_transport_ptr_t) (long)sock);
+	gnutls_transport_set_ptr(*session, (gnutls_transport_ptr_t) GINT_TO_POINTER(sock));
 	ret = gnutls_handshake(*session);
 	if (ret < 0) {
 		fprintf(stderr, "*** Handshake has failed (%s)\n\n",
