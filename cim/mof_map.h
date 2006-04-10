79a63cd70ae649cb5e0f6327fb4a3d57ccbdfe7d
panjiam <devnull@localhost>
2006-04-10 10:15:21 +0000

    moved map tables into mof_map.c
    CVS patchset: 9203
    CVS date: 2006/04/10 10:15:21
    
    
    Mercurial revision: a283e9e7586830c06c055078dde68c8cf052c818

--- cim/mof_map.h
+++ cim/mof_map.h
@@ -1,108 +1,55 @@
+/*
+ * mof_map.h: map class properties to msg attributes
+ *
+ * Author: Jia Ming Pan <jmltc@cn.ibm.com>
+ * Copyright (c) 2005 International Business Machines
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ */
+
+
 #ifndef _MOF_MAP_H
 #define _MOF_MAP_H
 
-typedef struct mapping_t_s {
+typedef struct {
 	const char * key;
         const char * name;
         int	type;
-} mapping_t;
+} map_entry_t;
 
-#define MAPDIM(x)	(sizeof(x)/sizeof(mapping_t))
+struct map_t{
+	int	id;
+	int	len;
+	const 	map_entry_t *entry;
+};
 
-#define MAPPING_HA_Cluster 						\
-	{KEY_HBVERSION,	   "HBVersion",     CMPI_chars},		\
-	{KEY_HOST, 	   "Node",          CMPI_charsA},		\
-	{KEY_HOPS, 	   "HOPFudge",      CMPI_chars},		\
-	{KEY_KEEPALIVE,    "KeepAlive",     CMPI_chars},		\
-	{KEY_DEADTIME, 	   "DeadTime",      CMPI_chars},		\
-	{KEY_DEADPING, 	   "DeadPing",      CMPI_chars},		\
-	{KEY_WARNTIME, 	   "WarnTime",      CMPI_chars},		\
-	{KEY_INITDEAD, 	   "InitDead",      CMPI_chars},		\
-	{KEY_WATCHDOG, 	   "WatchdogTimer", CMPI_chars},		\
-	{KEY_BAUDRATE,	   "BaudRate",      CMPI_chars},		\
-	{KEY_UDPPORT,  	   "UDPPort",       CMPI_chars},		\
-	{KEY_FACILITY, 	   "LogFacility",   CMPI_chars},		\
-	{KEY_LOGFILE, 	   "LogFile",       CMPI_chars},		\
-	{KEY_DBGFILE,	   "DebugFile",     CMPI_chars},		\
-	{KEY_FAILBACK, 	   "NiceFailBack",  CMPI_chars},		\
-	{KEY_AUTOFAIL, 	   "AutoFailBack",  CMPI_chars},		\
-	{KEY_STONITH, 	   "Stonith",       CMPI_chars},		\
-	{KEY_STONITHHOST,  "StonithHost",   CMPI_chars},		\
-	{KEY_CLIENT_CHILD, "Respawn",       CMPI_chars},		\
-	{KEY_RT_PRIO, 	   "RTPriority",    CMPI_chars},		\
-	{KEY_GEN_METH, 	   "GenMethod",     CMPI_chars},		\
-	{KEY_REALTIME, 	   "RealTime",      CMPI_chars},		\
-	{KEY_DEBUGLEVEL,   "DebugLevel",    CMPI_chars},		\
-	{KEY_NORMALPOLL,   "NormalPoll",    CMPI_chars},		\
-	{KEY_APIPERM, 	   "APIAuth",       CMPI_charsA},		\
-	{KEY_MSGFMT, 	   "MsgFmt",        CMPI_chars},		\
-	{KEY_LOGDAEMON,    "UseLogd",       CMPI_chars},		\
-	{KEY_CONNINTVAL,   "ConnLogdTime",  CMPI_chars},		\
-	{KEY_BADPACK, 	   "LogBadPack",    CMPI_chars},		\
-	{KEY_REGAPPHBD,    "NormalPoll",    CMPI_chars},		\
-	{KEY_COREDUMP, 	   "CoreDump",      CMPI_chars},		\
-	{KEY_COREROOTDIR,  "CoreRootDir",   CMPI_chars},		\
-	{KEY_REL2, 	   "WithCrm",       CMPI_chars},		\
-	{"pingnode",       "PingNode",      CMPI_charsA},		\
-	{"pinggroup",      "PingGroup",     CMPI_charsA}	
-			
-#define MAPPING_HA_ClusterNode					\
-	{"uname",	"Name",		CMPI_chars},		\
-	{"online",	"OnLine",	CMPI_chars},		\
-        {"standby",	"Standby",	CMPI_chars},		\
-	{"unclean",	"Unclean",	CMPI_chars},		\
-	{"shutdown",	"Shutdown",	CMPI_chars},		\
-	{"expected_up",	"ExpectedUp",	CMPI_chars},		\
-	{"node_ping",	"NodePing",	CMPI_chars},		\
-	{"is_dc",	"IsDC",		CMPI_chars},
+const struct map_t *	cim_query_map(int mapid); 
 
-#define MAPPING_HA_PrimitiveResource				\
-	{"id",		"Id",		CMPI_chars},		\
-	{"class",	"ResourceClass",CMPI_chars},		\
-	{"type",	"Type",		CMPI_chars},		\
-	{"provider",	"Provider",	CMPI_chars},		\
-	{"groupid",	"GroupId",	CMPI_chars}
+#define HA_CLUSTER			1
+#define HA_CLUSTER_NODE			2
+#define HA_PRIMITIVE_RESOURCE		3
+#define HA_RESOURCE_CLONE		4
+#define HA_MASTERSLAVE_RESOURCE		5
+#define HA_OPERATION			6
+#define HA_ORDER_CONSTRAINT		7
+#define HA_COLOCATION_CONSTRAINT	8
+#define HA_LOCATION_CONSTRAINT		9
+#define HA_RESOURCE_GROUP		10
 
-
-#define MAPPING_HA_ResourceClone 				\
-	{"id",		"Id",		CMPI_chars},		\
-        {"notify",	"Notify",	CMPI_chars},		\
-        {"ordered",	"Ordered",	CMPI_chars},		\
-        {"interleave",	"Interleave",	CMPI_chars},		\
-        {"clone_max",	"CloneMax",	CMPI_chars},		\
-        {"clone_node_max","CloneNodeMax",CMPI_chars}
-
-#define MAPPING_HA_MasterSlaveResource				\
-	{"id",			"Id",		CMPI_chars},	\
-        {"clone_max",        	"CloneMax",       CMPI_chars},	\
-        {"clone_node_max",   	"CloneNodeMax",   CMPI_chars},	\
-        {"master_max",	     	"MaxMasters",     CMPI_chars},	\
-        {"master_node_max", 	"MaxNodeMasters", CMPI_chars}
-
-#define MAPPING_HA_Operation					\
-	{"id",		"Id",		CMPI_chars},		\
-	{"name",	"Name",		CMPI_chars},		\
-	{"interval",	"Interval",	CMPI_chars},		\
-	{"timeout",	"Timeout",	CMPI_chars}
-
-#define MAPPING_HA_OrderConstraint				\
-	{"id",		"Id",		CMPI_chars},		\
-	{"from",	"From",		CMPI_chars},		\
-	{"type",	"OrderType",	CMPI_chars},		\
-        {"",		"Action",	CMPI_chars},		\
-        {"to",		"To",		CMPI_chars},		\
-        {"",		"Symmetrical",  CMPI_chars}
-
-
-#define MAPPING_HA_ColocationConstraint				\
-	{"id",		"Id",		CMPI_chars},		\
-	{"from",	"From",		CMPI_chars},		\
-        {"to",		"To",		CMPI_chars},		\
-        {"score",	"Score",	CMPI_chars}
-
-#define MAPPING_HA_LocationConstraint				\
-	{"id",		"Id",		CMPI_chars},		\
-        {"score",	"Score",	CMPI_chars},		\
-        {"resource",	"Resource",	CMPI_chars}
+#define METHOD_ADD_RESOURCE		"AddResource"
+#define METHOD_ADD_PRIMITIVE_RESOURCE	"AddPrimitiveResource"
 
 #endif
