c244a6b890765c52948ba8c7e751aa8e0843643c
panjiam <devnull@localhost>
2006-04-27 08:11:38 +0000

    order constraint and colocation constraint creation tests
    CVS patchset: 9430
    CVS date: 2006/04/27 08:11:38
    
    
    Mercurial revision: 26ff9d7c1acde7c4b1e22c44ad32daf494312c92

--- cim/tests/ResourceTest.sh
+++ cim/tests/ResourceTest.sh
@@ -169,16 +169,56 @@ SystemCreationClassName="HA_LinuxHA"
 }
 
 
+function create_order_constraint() 
+{
+CONS_ID=$1
+FROM=$2
+TO=$3
+TYPE=$4
+
+echo "creating order_constraint $CONS_ID, $FROM, $TO, $TYPE"
+wbemcli ci http://$USER:$PASSWD@localhost/root/cimv2:HA_OrderConstraint.Id="$CONS_ID",\
+CreationClassName="HA_OrderConstraint",\
+SystemName="LinuxHACluster",\
+SystemCreationClassName="HA_LinuxHA" \
+Id="$CONS_ID",\
+CreationClassName="HA_OrderConstraint",\
+SystemName="LinuxHACluster",\
+SystemCreationClassName="HA_LinuxHA",\
+From="$FROM",\
+To="$TO",\
+OrderType=$TYPE
+} 
+
+function create_colocation_constraint() 
+{
+CONS_ID=$1
+FROM=$2
+TO=$3
+SCORE=$4
+
+echo "creating colocation_constraint $CONS_ID, $FROM, $TO, $SCORE"
+wbemcli ci http://$USER:$PASSWD@localhost/root/cimv2:HA_ColocationConstraint.Id="$CONS_ID",\
+CreationClassName="HA_ColocationConstraint",\
+SystemName="LinuxHACluster",\
+SystemCreationClassName="HA_LinuxHA" \
+Id="$CONS_ID",\
+CreationClassName="HA_ColocationConstraint",\
+SystemName="LinuxHACluster",\
+SystemCreationClassName="HA_LinuxHA",\
+From="$FROM",\
+To="$TO",\
+Score=$SCORE
+} 
+
 ##############################################################
 # primitive resource
 ##############################################################
-
 echo "---------------------------------------------------"
 echo "Primitive Resource Creation test"
 echo "---------------------------------------------------"
 RESOURCE_ID=test_primitive_resource
 ATTRIBUTE_ID=${RESOURCE_ID}_ip
-
 delete_resource HA_PrimitiveResource $RESOURCE_ID 2>/dev/null
 create_primitive_resource "IPaddr" "$RESOURCE_ID"
 create_attribute $RESOURCE_ID $ATTRIBUTE_ID "ip" "127.0.0.111"
@@ -187,29 +227,45 @@ cib_add_resource $RESOURCE_ID
 create_attribute $RESOURCE_ID ${RESOURCE_ID}_netmask "netmask" "255.255.255.0"
 create_operation $RESOURCE_ID ${RESOURCE_ID}_monitor "monitor" "10s" "20s"
 wait_cib_updated
-
-rc=0
 resource_query $RESOURCE_ID
-resource_query $RESOURCE_ID | grep "$RESOURCE_ID">/dev/null \
-	|| { echo "[FAILED] $RESOURCE_ID not found." && rc=1; }
-resource_query $RESOURCE_ID | grep "$ATTRIBUTE_ID">/dev/null\
-	|| { echo "[FAILED] $ATTRIBUTE_ID not found." && rc=1; }
-if [ $rc = 0 ]; then 
-	echo "[OK] create resource group:$GROUP_ID successfully."
-fi
 
+echo "---------------------------------------------------"
+echo "Primitive Resource Creation test 2"
+echo "---------------------------------------------------"
+RESOURCE_ID2=test_primitive_resource_2
+ATTRIBUTE_ID2=${RESOURCE_ID2}_ip
+delete_resource HA_PrimitiveResource $RESOURCE_ID2 2>/dev/null
+create_primitive_resource "IPaddr" "$RESOURCE_ID2"
+create_attribute $RESOURCE_ID2 $ATTRIBUTE_ID2 "ip" "127.0.0.112"
+cib_add_resource $RESOURCE_ID2
+wait_cib_updated
+resource_query $RESOURCE_ID
 
-#delete_resource HA_PrimitiveResource $RESOURCE_ID
 echo "---------------------------------------------------"
 echo "Location Constraint creation test"
 echo "---------------------------------------------------"
 delete_constraint HA_LocationConstraint ${RESOURCE_ID}_location_cons
 create_location_constraint ${RESOURCE_ID}_location_cons ${RESOURCE_ID} "node1" "1000"
+wait_cib_updated
+
+echo "---------------------------------------------------"
+echo "Location Constraint creation test"
+echo "---------------------------------------------------"
+delete_constraint HA_OrderConstraint test_order_cons
+create_order_constraint test_order_cons ${RESOURCE_ID} ${RESOURCE_ID2} "after"
+wait_cib_updated
+
+
+echo "---------------------------------------------------"
+echo "Colocation Constraint creation test"
+echo "---------------------------------------------------"
+delete_constraint HA_ColocationConstraint test_colocation_cons
+create_colocation_constraint test_colocation_cons ${RESOURCE_ID} ${RESOURCE_ID2} 1000
+wait_cib_updated
 
 #############################################################
 # resource group
 #############################################################
-
 GROUP_ID=test_resource_group
 delete_resource HA_ResourceGroup $GROUP_ID 2>/dev/null
 
@@ -218,7 +274,6 @@ echo "Resource Group Creation test"
 echo "---------------------------------------------------"
 SUB_RESOURCE_ID=sub_resource_1
 SUB_ATTRIBUTE_ID=${SUB_RESOURCE_ID}_ip
-
 create_primitive_resource "IPaddr" "$SUB_RESOURCE_ID"
 create_attribute $SUB_RESOURCE_ID $SUB_ATTRIBUTE_ID "ip" "127.0.0.111"
 create_resource_group $GROUP_ID
@@ -227,17 +282,6 @@ cib_add_resource $GROUP_ID
 wait_cib_updated
 resource_query $GROUP_ID
 
-rc=0
-resource_query $GROUP_ID | grep "$GROUP_ID" > /dev/null		\
-	|| { echo "[FAILED] $GROUP_ID not found in CIB." && rc=1; }
-resource_query $GROUP_ID | grep "$SUB_RESOURCE_ID" >/dev/null	\
-	|| { echo "[FAILED] $SUB_RESOURCE_ID not found in CIB." && rc=1; }
-resource_query $GROUP_ID | grep "$SUB_ATTRIBUTE_ID" >/dev/null	\
-	|| { echo "[FAILED] $SUB_ATTRIBUTE_ID not found in CIB." && rc=1; }
-
-if [ $rc = 0 ]; then 
-	echo "[OK] create resource group:$GROUP_ID successfully."
-fi
 echo "---------------------------------------------------"
 echo "Resource Group Add Resource test"
 echo "---------------------------------------------------"
@@ -249,10 +293,4 @@ create_attribute $SUB_RESOURCE_ID2 $SUB_ATTRIBUTE_ID2 "ip" "127.0.0.112"
 group_add_resource $GROUP_ID $SUB_RESOURCE_ID2
 wait_cib_updated
 resource_query $GROUP_ID
-resource_query $GROUP_ID | grep $SUB_RESOURCE_ID2 >/dev/null \
-        || { echo "[FAILED] $SUB_RESOURCE_ID2 not found in CIB." && rc=1; }
-
-#delete_resource HA_ResourceGroup $GROUP_ID
-
-
 
