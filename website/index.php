e2377863f37de60e7405a42f7b11b59b7a16c2a7
alan <devnull@localhost>
2006-05-17 16:52:36 +0000

    Put in the Javascript-to-select-CSS change.
    CVS patchset: 9582
    CVS date: 2006/05/17 16:52:36
    
    
    Mercurial revision: 2013c215c720662ce3d2e5eeb4f027b365aa9266

--- website/index.php
+++ website/index.php
@@ -11,10 +11,19 @@ Copyright:    (C) 2005 Dmytri Kleiner <dk@trick.ca>
  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
  <meta http-equiv="keywords"
        content="high-availability, open source software, free software, POSIX, UNIX, FreeBSD, Solaris, cluster, reliability, availability, serviceability">
+<script type="text/javascript">
+  if ((navigator.appName).indexOf("Microsoft") != -1) {
+    document.write('<link rel="stylesheet" href="/linuxhaIE6.css" type="text/css">');
+  }else{
+    document.write('<link rel="stylesheet" href="/linuxha.css" type="text/css">');
+  }
+</script>
+<noscript>
+  <link rel="stylesheet" href="/linuxha.css" type="text/css">
+</noscript>
  <meta name="author" content="wiki dot linux dash ha dot org">
  <?php robots_metadata(); ?>
  <title><?php echo "$pagetitle: $sitename"; ?></title>
- <?php stylesheet_link(); ?>
 </head>
 <body>
 <?php browser_compatibility_messages(); ?>
