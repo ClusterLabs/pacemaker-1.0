1e4f5012c633d71ba03d1a8731782c7484576d08
Andrew Beekhof <beekhof@suse.de>
2008-04-01 10:11:45 +0200

    Low: Contrib: showscores.sh - Update from Dominik Klein
    Mercurial revision: 8330900c0ef20079fdf6dfbc0019e60700469fdb

--- contrib/showscores.sh
+++ contrib/showscores.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Dec 2007, Dominik Klein
+# Mar 2008, Dominik Klein
 # Display scores of Linux-HA resources
 
 # Known issues:
@@ -12,14 +12,23 @@ if [ `crmadmin -D | cut -d' ' -f4` != `uname -n|tr "[:upper:]" "[:lower:]"` ]
   then echo "Warning: Script running not on DC. Might be slow(!)"
 fi
 
-# Heading
-printf "%-16s%-16s%-16s%-16s%-16s%-16s\n" "Score" "Resource" "Node" "Stickiness" "Failcount" "Failure-Stickiness"
+sortby=1
+[ -n "$1" ] && [ "$1" = "node" ] && sortby=3
 
-2>&1 ptest -LVVVVVVV|grep -E "assign_node|rsc_location"|grep -w -E "\ [-]{0,1}[0-9]*$"|while read line
+# Heading
+printf "%-20s%-10s%-16s%-11s%-9s%-16s\n" "Resource" "Score" "Node" "Stickiness" "#Fail" "Fail-Stickiness"
+
+#2>&1 ptest -LVVVVVVV|grep -E "assign_node|rsc_location"|grep -w -E "\ [-]{0,1}[0-9]*$"|while read line
+2>&1 ptest -LVVVVVVVV|grep -E "dump_node_scores"|grep -E "Post-merge|After"|grep -w -E "\ [-]{0,1}[0-9]*$"|while read line
 do
-	node=`echo $line|cut -d ' ' -f 8|cut -d ':' -f 1`
-	res=`echo $line|cut -d ' ' -f 6|tr -d ","`
-	score=`echo $line|cut -d ' ' -f 9|sed 's/1000000/INFINITY/g'`
+	#node=`echo $line|cut -d ' ' -f 8|cut -d ':' -f 1`
+	node=`echo $line|cut -d '.' -f 2 | cut -d " " -f 1`
+	node=`echo $line|cut -d "=" -f 1|sed 's/  *$//g'|grep -o -E "[^\ ]*$"|grep -o "[^\.]*$"|sed 's/\.$//'`
+	#res=`echo $line|cut -d ' ' -f 6|tr -d ","`
+	#res=`echo $line| cut -d '.' -f 1 | grep -o -E "[^\ ]*$"`
+	res=`echo $line|cut -d "=" -f 1|sed 's/  *$//g'|grep -o -E "[^\ ]*$"|grep -o "^.*\."|sed 's/\.$//'`
+	#score=`echo $line|cut -d ' ' -f 9|sed 's/1000000/INFINITY/g'`
+	score=`echo $line|grep -o -E "[-0-9]*$"|sed 's/1000000/INFINITY/g'`
 
 	# get meta attribute resource_stickiness
 	if ! stickiness=`crm_resource -g resource_stickiness -r $res --meta 2>/dev/null`
@@ -34,12 +43,22 @@ do
 
 	# get meta attribute resource_failure_stickiness
 	if ! failurestickiness=`crm_resource -g resource_failure_stickiness -r $res --meta 2>/dev/null`
-	then 
+	then
 		# if that doesnt exist, get the default value
 		failurestickiness=`cibadmin -Q -o crm_config 2>/dev/null|grep "resource[_-]failure[_-]stickiness"|grep -o -E 'value ?= ?"[^ ]*"'|cut -d '"' -f 2|grep -v "^$"`
 	fi	
 
-	failcount=`crm_failcount -G -r $res -U $node 2>/dev/null|grep -o -E 'value ?= ?[0-9]*'|cut -d '=' -f 2|grep -v "^$"`
+	#failcount=`crm_failcount -G -r $res -U $node 2>/dev/null|grep -o -E 'value ?= ?[0-9]*|[+-]INFINITY|[+-]infinity'|cut -d '=' -f 2|grep -v "^$"`
+	failcount=`crm_failcount -G -r $res -U $node 2>/dev/null|grep -o -E 'value ?= ?INFINITY|value ?= ?[0-9]*'|cut -d '=' -f 2|grep -v "^$"`
 
-	printf "%-16s%-16s%-16s%-16s%-16s%-16s\n" $res $score $node $stickiness $failcount $failurestickiness
-done|sort -k 1
+	if echo $line | grep -q After
+	then
+		res=$res"_(master)"
+	fi
+	if echo $res | grep -q -E ':[0-9]{1,2}'
+	then
+		res=$res"_(clone)"
+	fi
+
+	printf "%-20s%-10s%-16s%-11s%-9s%-16s\n" $res $score $node $stickiness $failcount $failurestickiness
+done|sort -k $sortby
