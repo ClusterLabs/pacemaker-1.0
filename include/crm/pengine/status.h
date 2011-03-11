7bca0eb0ec6729e7f1ea1d803d608458cef3350e
Yan Gao <ygao@novell.com>
2011-03-11 14:53:36 +0800

    Medium: PE: Indicate resource failures even if on-fail="ignore" (transplanted from 3108f2ead907d130a76c9ff6ea6963ada2ca8063)
    Mercurial revision: 579becc138d459ec05d27875e43859606e97faa5

--- include/crm/pengine/status.h
+++ include/crm/pengine/status.h
@@ -154,6 +154,8 @@ struct node_s {
 #define pe_rsc_starting		0x00100000ULL
 #define pe_rsc_stopping		0x00200000ULL
 
+#define pe_rsc_failure_ignored  0x01000000ULL
+
 struct resource_s { 
 		char *id; 
 		char *clone_name; 
