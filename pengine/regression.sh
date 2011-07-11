255df6dc16cc602a4bd6a8a65ae81857f940c416
Gao,Yan <ygao@suse.com>
2011-07-11 21:00:25 +0800

    Low: PE: Bug lf#2606 - A demote operation is implied by the stonith operation (regression test) (transplanted from 0bffc2e25342fd87ad766874aecc83c5c92b6bbe)
    Mercurial revision: fde311c87dff3fd12febc9f68dd7cdc21f6e793f

--- pengine/regression.sh
+++ pengine/regression.sh
@@ -331,6 +331,7 @@ do_test bug-lf-1920 "Correctly handle probes that find active resources"
 do_test bnc-515172 "Location constraint with multiple expressions"
 do_test colocate-primitive-with-clone "Optional colocation with a clone"
 do_test bug-lf-2551 "STONITH ordering for stop"
+do_test bug-lf-2606 "Stonith implies demote"
 
 echo ""
 do_test systemhealth1  "System Health ()               #1"
