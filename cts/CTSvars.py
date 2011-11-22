0d29e414ff84feb5784e97b89a8be2cf35ff664f
Andrew Beekhof <andrew@beekhof.net>
2011-11-22 13:51:43 +1100

    Low: cts: Remove more build artifacts

--- cts/CTSvars.py
+++
@@ -1,8 +0,0 @@
-class CTSvars:
-    CTS_home="/usr/share/pacemaker/tests/cts"
-    CRM_CONFIG_DIR="/var/lib/heartbeat/crm"
-    CRM_DAEMON_USER="hacluster"
-    CRM_DAEMON_DIR="/usr/lib64/heartbeat"
-    HA_VARLIBHBDIR="/var/lib/heartbeat"
-    OCF_ROOT_DIR="/usr/lib/ocf"
-    INITDIR="/etc/init.d"
