ClusterLabs/pacemaker

High: PE: Fix coverity REVERSE_INULL defects

lib/pengine/status.c
@@ -147,10 +147,10 @@ pe_free_nodes(GListPtr nodes)
         iterator = iterator->next;
 
         crm_trace("deleting node");
-        crm_trace("%s is being deleted", details->uname);
         print_node("delete", node, FALSE);
 
         if (details != NULL) {
+            crm_trace("%s is being deleted", details->uname);
             if (details->attrs != NULL) {
                 g_hash_table_destroy(details->attrs);
             }

pengine/native.c
@@ -1606,10 +1606,6 @@ native_rsc_location(resource_t * rsc, rsc_to_node_t * constraint)
     GHashTableIter iter;
     node_t *node = NULL;
 
-    crm_trace("Applying %s (%s) to %s", constraint->id,
-                role2text(constraint->role_filter), rsc->id);
-
-    /* take "lifetime" into account */
     if (constraint == NULL) {
         pe_err("Constraint is NULL");
         return;
@@ -1617,8 +1613,13 @@ native_rsc_location(resource_t * rsc, rsc_to_node_t * constraint)
     } else if (rsc == NULL) {
         pe_err("LHS of rsc_to_node (%s) is NULL", constraint->id);
         return;
+    }
 
-    } else if (constraint->role_filter > 0 && constraint->role_filter != rsc->next_role) {
+    crm_trace("Applying %s (%s) to %s", constraint->id,
+                role2text(constraint->role_filter), rsc->id);
+    
+    /* take "lifetime" into account */
+    if (constraint->role_filter > 0 && constraint->role_filter != rsc->next_role) {
         crm_debug("Constraint (%s) is not active (role : %s)",
                   constraint->id, role2text(constraint->role_filter));
         return;

ClusterLabs/pacemaker-1.0

Backported from: ClusterLabs/pacemaker@46ef4b136ddcf179b6772bb260b55c5b6add1f92

lib/pengine/status.c
@@ -149,10 +149,10 @@ pe_free_nodes(GListPtr nodes)
 		iterator = iterator->next;
 
 		crm_debug_5("deleting node");
-		crm_debug_5("%s is being deleted", details->uname);
 		print_node("delete", node, FALSE);
 		
 		if(details != NULL) {
+			crm_debug_5("%s is being deleted", details->uname);
 			if(details->attrs != NULL) {
 				g_hash_table_destroy(details->attrs);
 			}

pengine/native.c
@@ -1035,10 +1035,6 @@ void native_rsc_location(resource_t *rsc, rsc_to_node_t *constraint)
 {
 	GListPtr or_list;
 
-	crm_debug_2("Applying %s (%s) to %s", constraint->id,
-		    role2text(constraint->role_filter), rsc->id);
-
-	/* take "lifetime" into account */
 	if(constraint == NULL) {
 		pe_err("Constraint is NULL");
 		return;
@@ -1046,9 +1042,14 @@ void native_rsc_location(resource_t *rsc, rsc_to_node_t *constraint)
 	} else if(rsc == NULL) {
 		pe_err("LHS of rsc_to_node (%s) is NULL", constraint->id);
 		return;
+	}
 
-	} else if(constraint->role_filter > 0
-		  && constraint->role_filter != rsc->next_role) {
+	crm_debug_2("Applying %s (%s) to %s", constraint->id,
+		    role2text(constraint->role_filter), rsc->id);
+
+	/* take "lifetime" into account */
+	if(constraint->role_filter > 0
+	   && constraint->role_filter != rsc->next_role) {
 		crm_debug("Constraint (%s) is not active (role : %s)",
 			  constraint->id, role2text(constraint->role_filter));
 		return;