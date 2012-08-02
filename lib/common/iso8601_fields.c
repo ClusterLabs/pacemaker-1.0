ClusterLabs/pacemaker

Low: tools: Bug cl#5032 - Fixes issue in iso8601 calculations.
The number of days in a month is not the same for every month.
This was not taken into account correctly when adding and subtracting
days.  When adding or subtracting days that results in changing to
another month, the previous month's number of days would be used
rather than the updated month's number of days when calculating the new
date.  For example, rolling back a day from Feb 01 would result in
Jan 28 instead of Jan 31. This patch addresses this by updating the
number of days in the current month dynamically as days are added
and subtracted. This also fixes an off by one error in the do_sub_field
macro which also attributed to calculations being inaccurate in the
reported issue.

lib/common/iso8601_fields.c
@@ -43,6 +43,24 @@
 		crm_trace("Result: %d", atime->field);		\
 	}
 
+#define do_add_days_field(atime, field, extra, overflow)		\
+	{								\
+		int __limit = days_per_month(atime->months, atime->years);	\
+		crm_trace("Adding %d to %d (limit=%d)",		\
+			    extra, atime->field, __limit);		\
+		atime->field += extra;					\
+		if(__limit > 0) {						\
+			while(__limit < atime->field) {			\
+				crm_trace("Overflowing: %d", atime->field); \
+				overflow(atime, 1);			\
+				__limit = days_per_month(atime->months, atime->years);	\
+				atime->field -= __limit;			\
+			}						\
+		}							\
+		atime->field = atime->field;				\
+		crm_trace("Result: %d", atime->field);		\
+	}
+
 #define do_add_time_field(atime, field, extra, limit, overflow)		\
 	{								\
 		crm_trace("Adding %d to %d (limit=%d)",		\
@@ -64,7 +82,7 @@
 		crm_trace("Subtracting %d from %d (limit=%d)",	\
 			    extra, atime->field, limit);		\
 		atime->field -= extra;					\
-		while(atime->field <= 1) {				\
+		while(atime->field < 1) {				\
 			crm_trace("Underflowing: %d", atime->field);	\
 			atime->field += limit;				\
 			overflow(atime, 1);				\
@@ -72,6 +90,20 @@
 		crm_trace("Result: %d", atime->field);		\
 	}
 
+#define do_sub_days_field(atime, field, extra, overflow)		\
+	{								\
+		int __limit = days_per_month(atime->months, atime->years);	\
+		crm_trace("Subtracting %d from %d (__limit=%d)",	\
+			    extra, atime->field, __limit);		\
+		atime->field -= extra;					\
+		while(atime->field < 1) {				\
+			crm_trace("Underflowing: %d", atime->field);	\
+			overflow(atime, 1);				\
+			__limit = days_per_month(atime->months, atime->years);	\
+			atime->field += __limit;				\
+		}							\
+		crm_trace("Result: %d", atime->field);		\
+	}
 #define do_sub_time_field(atime, field, extra, limit, overflow)		\
 	{								\
 		crm_trace("Subtracting %d from %d (limit=%d)",	\
@@ -125,8 +157,7 @@ add_days(ha_time_t * a_time, int extra)
     if (extra < 0) {
         sub_days(a_time, -extra);
     } else {
-        do_add_field(a_time, days, extra,
-                     days_per_month(a_time->months, a_time->years), add_months);
+        do_add_days_field(a_time, days, extra, add_months);
     }
 
     convert_from_gregorian(a_time);
@@ -280,8 +311,7 @@ sub_days(ha_time_t * a_time, int extra)
     if (extra < 0) {
         add_days(a_time, -extra);
     } else {
-        do_sub_field(a_time, days, extra,
-                     days_per_month(a_time->months, a_time->years), sub_months);
+        do_sub_days_field(a_time, days, extra, sub_months);
     }
 
     convert_from_gregorian(a_time);

ClusterLabs/pacemaker-1.0

Backported from: ClusterLabs/pacemaker@f4f6c27d66f0538094ff0e585d82b1d086ed0ea8

lib/common/iso8601_fields.c
@@ -44,6 +44,24 @@
 		crm_debug_6("Result: %d", atime->field);		\
 	}
 
+#define do_add_days_field(atime, field, extra, overflow)		\
+	{								\
+		int __limit = days_per_month(atime->months, atime->years);	\
+		crm_debug_6("Adding %d to %d (limit=%d)",		\
+			    extra, atime->field, __limit);		\
+		atime->field += extra;					\
+		if(__limit > 0) {						\
+			while(__limit < atime->field) {			\
+				crm_debug_6("Overflowing: %d", atime->field); \
+				overflow(atime, 1);			\
+				__limit = days_per_month(atime->months, atime->years);	\
+				atime->field -= __limit;			\
+			}						\
+		}							\
+		atime->field = atime->field;				\
+		crm_debug_6("Result: %d", atime->field);		\
+	}
+
 #define do_add_time_field(atime, field, extra, limit, overflow)		\
 	{								\
 		crm_debug_6("Adding %d to %d (limit=%d)",		\
@@ -65,7 +83,7 @@
 		crm_debug_6("Subtracting %d from %d (limit=%d)",	\
 			    extra, atime->field, limit);		\
 		atime->field -= extra;					\
-		while(atime->field <= 1) {				\
+		while(atime->field < 1) {				\
 			crm_debug_6("Underflowing: %d", atime->field);	\
 			atime->field += limit;				\
 			overflow(atime, 1);				\
@@ -73,6 +91,20 @@
 		crm_debug_6("Result: %d", atime->field);		\
 	}
 
+#define do_sub_days_field(atime, field, extra, overflow)		\
+	{								\
+		int __limit = days_per_month(atime->months, atime->years);	\
+		crm_debug_6("Subtracting %d from %d (__limit=%d)",	\
+			    extra, atime->field, __limit);		\
+		atime->field -= extra;					\
+		while(atime->field < 1) {				\
+			crm_debug_6("Underflowing: %d", atime->field);	\
+			overflow(atime, 1);				\
+			__limit = days_per_month(atime->months, atime->years);	\
+			atime->field += __limit;				\
+		}							\
+		crm_debug_6("Result: %d", atime->field);		\
+	}
 #define do_sub_time_field(atime, field, extra, limit, overflow)		\
 	{								\
 		crm_debug_6("Subtracting %d from %d (limit=%d)",	\
@@ -128,9 +160,7 @@ add_days(ha_time_t *a_time, int extra)
 	if(extra < 0) {
 		sub_days(a_time, -extra);
 	} else {
-		do_add_field(a_time, days, extra,
-			     days_per_month(a_time->months, a_time->years),
-			     add_months);
+		do_add_days_field(a_time, days, extra, add_months);
 	}
 	
 	convert_from_gregorian(a_time);
@@ -286,9 +316,7 @@ sub_days(ha_time_t *a_time, int extra)
 	if(extra < 0) {
 		add_days(a_time, -extra);
 	} else {
-		do_sub_field(a_time, days, extra,
-			     days_per_month(a_time->months, a_time->years),
-			     sub_months);
+		do_sub_days_field(a_time, days, extra, sub_months);
 	}
 
 	convert_from_gregorian(a_time);