ClusterLabs/pacemaker

Low: crmd: Fix log message

crmd/main.c
@@ -78,7 +78,7 @@ main(int argc, char **argv)
 
     crm_log_init(NULL, LOG_INFO, TRUE, FALSE, argc, argv);
 
-    crm_notice("CRM Hg Version: %s\n", BUILD_VERSION);
+    crm_notice("CRM Git Version: %s\n", BUILD_VERSION);
 
     if (optind > argc) {
         ++argerr;

ClusterLabs/pacemaker-1.0

Backported from: ClusterLabs/pacemaker@78648f059d962a84f3d12a9ddce28a31fd142508

crmd/main.c
@@ -79,7 +79,7 @@ main(int argc, char ** argv)
 
     crm_log_init(CRM_SYSTEM_CRMD, LOG_INFO, TRUE, FALSE, argc, argv);
     
-    crm_info("CRM Hg Version: %s\n", BUILD_VERSION);
+    crm_info("CRM Git Version: %s\n", BUILD_VERSION);
     
     if (optind > argc) {
 	    ++argerr;