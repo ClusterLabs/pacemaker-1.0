ClusterLabs/pacemaker

High: PE: Fix use-after-free errors detected by coverity

lib/pengine/utils.c
@@ -336,8 +336,6 @@ custom_action(resource_t * rsc, char *key, const char *task,
     }
 
     if (possible_matches != NULL) {
-        free(key);
-
         if (g_list_length(possible_matches) > 1) {
             pe_warn("Action %s for %s on %s exists %d times",
                     task, rsc ? rsc->id : "<NULL>",
@@ -509,6 +507,8 @@ custom_action(resource_t * rsc, char *key, const char *task,
             }
         }
     }
+
+    free(key);
     return action;
 }
 

ClusterLabs/pacemaker-1.0

Backported from: ClusterLabs/pacemaker@6a0d5dcc631887c9c77aea0c0741f4e91b7fc493
High: PE: Fix additional use-after-free introduced in cs: 6a0d5dc - Fix use-after-free errors detected by coverity
Backported from: ClusterLabs/pacemaker@1adc3a792d7f4edb2c09e4da71b36dce99ad2d5a
Backport Notes:
 - Above two commits are merged into this single commit.

lib/pengine/utils.c
@@ -445,8 +445,6 @@ custom_action(resource_t *rsc, char *key, const char *task,
 	}
 	
 	if(possible_matches != NULL) {
-		crm_free(key);
-		
 		if(g_list_length(possible_matches) > 1) {
 			pe_warn("Action %s for %s on %s exists %d times",
 				task, rsc?rsc->id:"<NULL>",
@@ -480,7 +478,7 @@ custom_action(resource_t *rsc, char *key, const char *task,
 		if(on_node) {
 		    action->node = node_copy(on_node);
 		}
-		action->uuid = key;
+		action->uuid = crm_strdup(key);
 		
 		action->actions_before   = NULL;
 		action->actions_after    = NULL;
@@ -622,6 +620,8 @@ custom_action(resource_t *rsc, char *key, const char *task,
 			}
 		}
 	}
+
+	crm_free(key);
 	return action;
 }
 