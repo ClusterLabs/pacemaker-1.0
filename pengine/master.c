ClusterLabs/pacemaker

Medium: PE: Observe the promotion score of probed resources

pengine/master.c
@@ -410,14 +410,18 @@ master_score(resource_t * rsc, node_t * node, int not_set_value)
         return score;
     }
 
-    if (rsc->fns->state(rsc, TRUE) < RSC_ROLE_STARTED) {
-        return score;
-    }
+    if (node == NULL) {
+        if(rsc->fns->state(rsc, TRUE) < RSC_ROLE_STARTED) {
+            crm_trace("Ingoring master score for %s: unknown state on %s",
+                      rsc->id, node->details->uname);
+            return score;
+        }
 
-    if (node != NULL) {
+    } else {
         node_t *match = pe_find_node_id(rsc->running_on, node->details->id);
+        node_t *known = pe_hash_table_lookup(rsc->known_on, node->details->id);
 
-        if (match == NULL) {
+        if (match == NULL && known == NULL) {
             crm_trace("%s is not active on %s - ignoring", rsc->id, node->details->uname);
             return score;
         }

ClusterLabs/pacemaker-1.0

Backported from: ClusterLabs/pacemaker@d7d8457a8911854b0b5b2a09f99326dea792752d
High: PE: Fix use-of-NULL in master_score() reported by coverity
Backported from: ClusterLabs/pacemaker@e1651e8136a7e027c4a0b8f5b64785d67132c5e2
Backport Notes:
 The test case master-probed-score has been re-generated for 1.0.
 The output .dot file looks different from 1.1 but they are considered same
 because the differences are only pseudo actions (fontcolor="orange").

pengine/master.c
@@ -373,13 +373,16 @@ master_score(resource_t *rsc, node_t *node, int not_set_value)
 	    return score;
 	}
 	
-	if(rsc->fns->state(rsc, TRUE) < RSC_ROLE_STARTED) {
-	    return score;
-	}
+	if (node == NULL) {
+	    if(rsc->fns->state(rsc, TRUE) < RSC_ROLE_STARTED) {
+		crm_debug_2("Ingoring master score for %s: unknown state", rsc->id);
+		return score;
+	    }
 
-	if(node != NULL) {
+	} else {
 	    node_t *match = pe_find_node_id(rsc->running_on, node->details->id);
-	    if(match == NULL) {
+	    node_t *known = pe_find_node_id(rsc->known_on, node->details->id);
+	    if (match == NULL && known == NULL) {
 		crm_debug_2("%s is not active on %s - ignoring",
 			    rsc->id, node->details->uname);
 		return score;