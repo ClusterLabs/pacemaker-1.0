ClusterLabs/pacemaker

High: crmd: Prevent use-of-NULL when free'ing empty hashtables

crmd/lrm.c
@@ -309,12 +309,18 @@ do_lrm_control(long long action,
             crm_info("Disconnected from the LRM");
         }
 
-        g_hash_table_destroy(resource_history);
-        resource_history = NULL;
-        g_hash_table_destroy(deletion_ops);
-        deletion_ops = NULL;
-        g_hash_table_destroy(pending_ops);
-        pending_ops = NULL;
+        if(resource_history) {
+            g_hash_table_destroy(resource_history);
+            resource_history = NULL;
+        }
+        if(deletion_ops) {
+            g_hash_table_destroy(deletion_ops);
+            deletion_ops = NULL;
+        }
+        if(pending_ops) {
+            g_hash_table_destroy(pending_ops);
+            pending_ops = NULL;
+        }
     }
 
     if (action & A_LRM_CONNECT) {

ClusterLabs/pacemaker-1.0

Backported from: ClusterLabs/pacemaker@d11c140841088984dee876c78b7b47a1f0debed8

crmd/lrm.c
@@ -139,12 +139,18 @@ do_lrm_control(long long action,
 		    crm_info("Disconnected from the LRM");
 		}
 
-		g_hash_table_destroy(resources);
-		resources = NULL;
-		g_hash_table_destroy(deletion_ops);
-		deletion_ops = NULL;
-		g_hash_table_destroy(pending_ops);
-		pending_ops = NULL;
+		if(resources) {
+		    g_hash_table_destroy(resources);
+		    resources = NULL;
+		}
+		if(deletion_ops) {
+		    g_hash_table_destroy(deletion_ops);
+		    deletion_ops = NULL;
+		}
+		if(pending_ops) {
+		    g_hash_table_destroy(pending_ops);
+		    pending_ops = NULL;
+		}
 	}
 
 	if(action & A_LRM_CONNECT) {