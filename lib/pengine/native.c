87775f01e393deab9951c690161317fd7f174209
Andrew Beekhof <andrew@beekhof.net>
2012-09-10 21:45:47 +1000

    Low: PE: Fix the output of failed resources
    Backported from: ClusterLabs/pacemaker@160e8477368db60fcfbebe0d442daa7617a7cd9a

--- lib/pengine/native.c
+++ lib/pengine/native.c
@@ -322,15 +322,15 @@ native_print(
 	if((options & pe_print_rsconly) || g_list_length(rsc->running_on) > 1) {
 		const char *desc = NULL;
 		desc = crm_element_value(rsc->xml, XML_ATTR_DESC);
-		status_print("%s%s\t(%s%s%s:%s%s) %s %s%s%s%s%s",
+		status_print("%s%s\t(%s%s%s:%s%s):\t%s %s%s%s%s%s",
 			     pre_text?pre_text:"", rsc->id,
 			     class, prov?"::":"", prov?prov:"", 
 			     crm_element_value(rsc->xml, XML_ATTR_TYPE),
 			     is_set(rsc->flags, pe_rsc_orphan)?" ORPHANED":"",
 			     (rsc->variant!=pe_native)?"":role2text(rsc->role),
-			     is_set(rsc->flags, pe_rsc_managed)?"":" (unmanaged)",
-			     is_set(rsc->flags, pe_rsc_failed)?" FAILED":"",
-			     is_set(rsc->flags, pe_rsc_failure_ignored)?" (failure ignored)":"",
+			     is_set(rsc->flags, pe_rsc_managed) ? "" : "(unmanaged) ",
+			     is_set(rsc->flags, pe_rsc_failed) ? "FAILED " : "",
+			     is_set(rsc->flags, pe_rsc_failure_ignored) ? "(failure ignored) " : "",
 			     desc?": ":"", desc?desc:"");
 
 	} else {
@@ -373,7 +373,7 @@ native_print(
 
 			   } else if((options & pe_print_printf)
 				     || (options & pe_print_ncurses)) {
-				   status_print("\t%s", node->details->uname);
+				   status_print(" %s", node->details->uname);
 
 			   } else if((options & pe_print_log)) {
 				   status_print("\t%d : %s",
